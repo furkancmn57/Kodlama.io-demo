af272a976e636b5e9321b9adfe5b4fbcd55bb4f7
Furkan Çimen <77547550+furkancmn57@users.noreply.github.com>
2024-03-24 17:40:23 +0300

    second commit

---
+++ Entities/Concretes/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public int CategoryId { get; set; }
+        public int InstructorId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public string CourseImage { get; set; }
+        public decimal CoursePrice { get; set; }
+        public bool CourseStatus { get; set; }
+    }
+}
---
+++ Entities/Concretes/Instructor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string ProfileImage { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Biography { get; set; }
+    }
+}
